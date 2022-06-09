3bba627a54b52c349542e977c3a89ab997bd30ea
l9-webserver <l9-webserver@hotmail.com>
2022-06-10 00:06:58 +0200

    tata sanićev

--- Assets/src/Game.cs
+++ Assets/src/Game.cs
@@ -7,6 +7,8 @@ using System.Web;
 using TMPro;
 using System.IO;
 using Newtonsoft.Json;
+
+#region klase
 [System.Serializable]
 public class ProvinceData
 {
@@ -67,8 +69,10 @@ public class WarData
     public string aggresor;
     public string defender;
 }
+#endregion
 public class Game : MonoBehaviour
 {
+    #region ui
     [Header("UI")]
     public Slider slider;
     public Slider sli_hue;
@@ -94,111 +98,71 @@ public class Game : MonoBehaviour
     public TextMeshProUGUI zup_vojska;
     public TextMeshProUGUI zup_stanovnistvo;
     public TextMeshProUGUI zup_text_name;
+    public TextMeshProUGUI t_plus;
     public RawImage map_mask;
     public Texture2D colourMap;
     public Texture2D fakeMap;
-
+    public Button plus_button;
+    #endregion
+    #region boja
     [Header("Boja")]
     public List<Color> prefered_colors;
     public Gradient odnosi_gradient;
     public Color highlight;
     public Color sel_high;
     public Gradient vojska_gradient;
-
+    #endregion
+    #region vektori
     [Header("Vektori")]
     public Vector2 map_size;
-
+    #endregion
+    #region objekti
     [Header("Objekti")]
     public Transform drzave_holder;
     public Transform ime_holder;
     public Transform ime_holder_zup;
+    public Transform zup_vojska_text_holder;
     public Transform objavi_rat_btn_transform;
     public GameObject template;
-
+    #endregion
+    #region konstante
     [Header("Konstante")]
     public int cena_vojnika = 5;
     public float loss_factor_recruit = 1.4f;
+    #endregion
+    #region globalne varijable
     [HideInInspector]
     public GameData gameData;
     Texture2D fakeMap_dum;
     Rect map_rect;
     Vector2 side_size;
     float scale_factor;
-    bool reset_highlight;
     string last_zup = "";
     string last_zup_selected;
+
+    int cena;
+
     Color last_zup_clr;
     Color last_zup_clr_selected;
     Color original;
     Color target;
-    bool og_set = false;
+
     Dictionary<Color, List<Vector2Int>> pixels_zup;
     Dictionary<Color, ProvinceData> provinces;
+    Dictionary<Color, Vector2Int> pozicije_misa_u_provinciji; //koordinate na ekranu, ne na teksturi
     Dictionary<string, CountryData> countries;
     Dictionary<string, EthnicData> ethnic_data;
     Dictionary<string, EthnicData> ethnic_data_zup;
     List<ProvinceData> selected_provinces;
     ProvinceData current_province;
     ProvinceData selected_province;
+
     bool changed_sliders = false;
-    int cena;
-    bool objavi_rat_bool;
-    string format_number(int num)
-    {
-        return string.Format("{0:N0}", num);
-    }
-    void change_province_owner(ProvinceData data, string owner)
-    {
-        provinces[data.color].country = owner;
-        update_provinces();
-    }
-    void set_objavi_rat_interactable(bool interactable)
-    {
-        objavi_rat_btn_transform.GetComponent<Button>().interactable = interactable;
-    }
-    void set_objavi_rat_text_alpha(float a)
-    {
-        Color clr = objavi_rat_btn_transform.GetChild(0).GetComponent<TextMeshProUGUI>().color;
-        objavi_rat_btn_transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = new Color(clr.r, clr.g, clr.b, a);
-    }
-    void update_potez_ui()
-    {
-        gore_levo_kinta.text = format_number(gameData.kinta);
-        gore_levo_potez.text = "Potez " + format_number(gameData.potez);
-        gore_levo_zarada.text = gameData.zarada > 0 ? String.Format("+{0:N0}", gameData.zarada) : format_number(gameData.zarada);
-        side_vojska.text = format_number(gameData.vojnici);
-    }
-    void update_province_ui()
-    {
-        zup_text_name.text = selected_province.name;
-        zup_stanovnistvo.text = $"{format_number(selected_province.stanovnici)} ljudi";
-        zup_vojska.text = $"{format_number(selected_province.vojska)} vojnika";
-    }
-    public void next_potez()
-    {
-        gameData.potez++;
-        gameData.kinta += gameData.zarada;
-        update_potez_ui();
-    }
-    void reset_fake_dum()
-    {
-        for (int x = 0; x < fakeMap.width; x++)
-        {
-            for (int y = 0; y < fakeMap.height; y++)
-            {
-                fakeMap_dum.SetPixel(x, y, fakeMap.GetPixel(x, y));
-            }
-        }
-    }
-    void erase_last_zup()
-    {
-        List<Vector2Int> pix_data = pixels_zup[last_zup_clr];
-        foreach (Vector2Int vector2Int in pix_data)
-        {
-            fakeMap_dum.SetPixel(vector2Int.x, vector2Int.y, Color.clear);
-        }
-        fakeMap_dum.Apply();
-    }
+    bool og_set = false;
+    bool objavi_rat_bool = false;
+    bool selected_province_is_part_of_country = false;
+    #endregion
+    #region glavne metode
     private void Start()
     {
         #region init
@@ -363,6 +327,231 @@ public class Game : MonoBehaviour
         update_rat_text();
         update_side_ui();
     }
+    void Update()
+    {
+        #region UI
+        if (slider.gameObject.activeSelf)
+        {
+            show_recruit();
+        }
+        if (sli_hue.transform.parent.gameObject.activeSelf)
+        {
+            show_color_pick();
+        }
+
+        #endregion
+        #region miš
+        Vector2 mouse_pos = Input.mousePosition;
+        if (map_rect.Contains(mouse_pos))
+        {
+            target = colourMap.GetPixel((int)mouse_pos.x - (int)side_size.x, (int)mouse_pos.y - (int)side_size.y);
+            if (provinces.ContainsKey(target))
+            {
+                #region na?i provinciju
+                current_province = provinces[target];
+                ProvinceData data = current_province;
+                CountryData countryData = countries[data.country];
+                if (data.name != last_zup && last_zup != "")
+                {
+                    erase_last_zup();
+                }
+                last_zup = data.name;
+                last_zup_clr = target;
+                #endregion
+
+                
+                if (Input.GetMouseButtonDown(0))
+                {
+                    #region pozicija miša na ekranu (provincija dictionary), ne teksturi
+                    Vector2Int mouse_pos_int = new Vector2Int( (int) mouse_pos.x, (int) mouse_pos.y);
+
+                    mouse_pos_int.x += (int) side_size.x;
+                    mouse_pos_int.y += (int) side_size.y;
+
+                    #endregion
+
+                    selected_province = current_province;
+                    selected_provinces.Add(selected_province);
+
+                    pozicije_misa_u_provinciji[selected_province.color] = mouse_pos_int;
+
+                    #region alpha promjene
+                    selected_province_is_part_of_country = selected_province.country == gameData.drzava;
+                    plus_button.interactable = selected_province_is_part_of_country;
+                    if (selected_province_is_part_of_country)
+                    {
+                        Color v_cpy = copy_color(side_vojska.color);
+                        v_cpy.a = 1f;
+
+                        side_vojska.color = v_cpy;
+
+                        v_cpy = copy_color(t_plus.color);
+                        v_cpy.a = 1f;
+
+                        t_plus.color = v_cpy;
+                    }
+                    else
+                    {
+                        Color v_cpy = copy_color(side_vojska.color);
+                        v_cpy.a = 0.5f;
+
+                        side_vojska.color = v_cpy;
+
+                        v_cpy = copy_color(t_plus.color);
+                        v_cpy.a = 0.5f;
+
+                        t_plus.color = v_cpy;
+
+                        
+                    }
+                    #endregion
+
+                    for (int i = 0; i < zup_vojska_text_holder.childCount; i++)
+                    {
+                        zup_vojska_text_holder.
+                    }
+
+                    foreach (Color key in pozicije_misa_u_provinciji.Keys)
+                    {
+                        
+                    }
+
+                    last_zup_selected = selected_province.name;
+                    last_zup_clr_selected = selected_province.color;
+
+                    update_side_ui();
+
+                    last_zup = ime.text;
+                    last_zup_clr = target;
+
+                    objavi_rat_bool = gameData.drzava == selected_province.country;
+                    set_objavi_rat_interactable(!objavi_rat_bool);
+
+                    #region boja UI elemenata
+                    set_objavi_rat_text_alpha(objavi_rat_bool ? 0.5f : 1f);
+
+                    odnosi_label.color = objavi_rat_bool ? new Color(odnosi_label.color.r, odnosi_label.color.g, odnosi_label.color.b, 0.5f) : new Color(odnosi_label.color.r, odnosi_label.color.g, odnosi_label.color.b, 1);
+                    odnosi.color = objavi_rat_bool ? new Color(odnosi.color.r, odnosi.color.g, odnosi.color.b, 0.5f) : new Color(odnosi.color.r, odnosi.color.g, odnosi.color.b, 1);
+                    #endregion
+                    
+                    EthnicData data_eth = ethnic_data[data.country];
+                    EthnicData data_eth_zup = ethnic_data_zup[data.name];
+
+                    #region etni?ki sastav UI
+                    for (int idxi = 0; idxi < data_eth.size; idxi++)
+                    {
+                        ime_holder.GetChild(idxi).GetComponent<TextMeshProUGUI>().text = String.Format("{0}: {1}%, {2:N0}", data_eth.ljudi[idxi], data_eth.postotci[idxi] * 100f, data_eth.stanovnici[idxi]);
+                    }
+                    for (int idxi = 0; idxi < data_eth_zup.size; idxi++)
+                    {
+                        ime_holder_zup.GetChild(idxi).GetComponent<TextMeshProUGUI>().text = String.Format("{0}: {1}%, {2:N0}", data_eth_zup.ljudi[idxi], data_eth_zup.postotci[idxi] * 100f, data_eth_zup.stanovnici[idxi]);
+                    }
+                    #endregion
+                    update_rat_text();
+                    update_province_ui();
+                }
+                List<Vector2Int> pix_data = pixels_zup[target];
+
+                foreach (Vector2Int vector2Int in pix_data)
+                {
+                    fakeMap_dum.SetPixel(vector2Int.x, vector2Int.y, highlight);
+                }
+                fakeMap_dum.Apply();
+                map_mask.texture = fakeMap_dum;
+            }
+        }
+        #endregion
+        #region GFX provincije
+        foreach (ProvinceData data in selected_provinces.ToArray())
+        {
+            if (data.name != selected_province.name)
+            {
+                List<Vector2Int> pix_data_ = pixels_zup[data.color];
+                foreach (Vector2Int vector2Int in pix_data_)
+                {
+                    fakeMap_dum.SetPixel(vector2Int.x, vector2Int.y, Color.clear);
+                }
+                fakeMap_dum.Apply();
+                map_mask.texture = fakeMap_dum;
+                selected_provinces.Remove(data);
+                continue;
+            }
+            else
+            {
+                List<Vector2Int> pix_data_ = pixels_zup[data.color];
+                foreach (Vector2Int vector2Int in pix_data_)
+                {
+                    fakeMap_dum.SetPixel(vector2Int.x, vector2Int.y, sel_high);
+                }
+                fakeMap_dum.Apply();
+                map_mask.texture = fakeMap_dum;
+                continue;
+            }
+        }
+        #endregion
+    }
+    #endregion
+    #region util metode
+    Color copy_color(Color og)
+    {
+        return new Color(og.r, og.g, og.b, og.a);
+    }
+    string format_number(int num)
+    {
+        return string.Format("{0:N0}", num);
+    }
+    void change_province_owner(ProvinceData data, string owner)
+    {
+        provinces[data.color].country = owner;
+        update_provinces();
+    }
+    void set_objavi_rat_interactable(bool interactable)
+    {
+        objavi_rat_btn_transform.GetComponent<Button>().interactable = interactable;
+    }
+    void set_objavi_rat_text_alpha(float a)
+    {
+        Color clr = objavi_rat_btn_transform.GetChild(0).GetComponent<TextMeshProUGUI>().color;
+        objavi_rat_btn_transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = new Color(clr.r, clr.g, clr.b, a);
+    }
+    void update_potez_ui()
+    {
+        gore_levo_kinta.text = format_number(gameData.kinta);
+        gore_levo_potez.text = "Potez " + format_number(gameData.potez);
+        gore_levo_zarada.text = gameData.zarada > 0 ? String.Format("+{0:N0}", gameData.zarada) : format_number(gameData.zarada);
+        side_vojska.text = format_number(gameData.vojnici);
+    }
+    void update_province_ui()
+    {
+        zup_text_name.text = selected_province.name;
+        zup_stanovnistvo.text = $"{format_number(selected_province.stanovnici)} ljudi";
+        zup_vojska.text = $"{format_number(selected_province.vojska)} vojnika";
+    }
+    public void next_potez()
+    {
+        gameData.potez++;
+        gameData.kinta += gameData.zarada;
+        update_potez_ui();
+    }
+    void reset_fake_dum()
+    {
+        for (int x = 0; x < fakeMap.width; x++)
+        {
+            for (int y = 0; y < fakeMap.height; y++)
+            {
+                fakeMap_dum.SetPixel(x, y, fakeMap.GetPixel(x, y));
+            }
+        }
+    }
+    void erase_last_zup()
+    {
+        List<Vector2Int> pix_data = pixels_zup[last_zup_clr];
+        foreach (Vector2Int vector2Int in pix_data)
+        {
+            fakeMap_dum.SetPixel(vector2Int.x, vector2Int.y, Color.clear);
+        }
+        fakeMap_dum.Apply();
+    }
     void show_recruit()
     {
         slider.maxValue = selected_province.homogenous_people;
@@ -450,8 +639,6 @@ public class Game : MonoBehaviour
         ProvinceData data = current_province;
         CountryData countryData = countries[data.country];
         ime.text = data.name;
-        last_zup = ime.text;
-        last_zup_clr = target;
         drzava.text = data.country;
         side_r_drzava.text = data.country;
         if (selected_province.country == gameData.drzava)
@@ -480,97 +667,9 @@ public class Game : MonoBehaviour
         }
         ime_zupanije.text = data.name;
     }
-    void Update()
+    public void resetiraj_slider()
     {
-        // If no click this frame, abort. Nothing to do.
-        if (slider.gameObject.activeSelf)
-        {
-            show_recruit();
-        }
-        if (sli_hue.transform.parent.gameObject.activeSelf)
-        {
-            show_color_pick();
-        }
-        Vector2 mouse_pos = Input.mousePosition;
-        if (map_rect.Contains(mouse_pos))
-        {
-            target = colourMap.GetPixel((int)mouse_pos.x - (int)side_size.x, (int)mouse_pos.y - (int)side_size.y);
-            //print(target);
-            if (provinces.ContainsKey(target))
-            {
-                current_province = provinces[target];
-                ProvinceData data = current_province;
-                CountryData countryData = countries[data.country];
-                if (data.name != last_zup && last_zup != "")
-                {
-                    erase_last_zup();
-                }
-                last_zup = data.name;
-                last_zup_clr = target;
-                if (Input.GetMouseButtonDown(0))
-                {
-                    selected_province = current_province;
-                    selected_provinces.Add(selected_province);
-                    last_zup_selected = selected_province.name;
-                    last_zup_clr_selected = selected_province.color;
-
-                    update_side_ui();
-
-                    objavi_rat_bool = gameData.drzava == selected_province.country;
-                    set_objavi_rat_interactable(!objavi_rat_bool);
-                    set_objavi_rat_text_alpha(objavi_rat_bool ? 0.5f : 1f);
-
-                    odnosi_label.color = objavi_rat_bool ? new Color(odnosi_label.color.r, odnosi_label.color.g, odnosi_label.color.b, 0.5f) : new Color(odnosi_label.color.r, odnosi_label.color.g, odnosi_label.color.b, 1);
-                    odnosi.color = objavi_rat_bool ? new Color(odnosi.color.r, odnosi.color.g, odnosi.color.b, 0.5f) : new Color(odnosi.color.r, odnosi.color.g, odnosi.color.b, 1);
-                    EthnicData data_eth = ethnic_data[data.country];
-                    EthnicData data_eth_zup = ethnic_data_zup[data.name];
-                    for (int idxi = 0; idxi < data_eth.size; idxi++)
-                    {
-                        ime_holder.GetChild(idxi).GetComponent<TextMeshProUGUI>().text = String.Format("{0}: {1}%, {2:N0}", data_eth.ljudi[idxi], data_eth.postotci[idxi] * 100f, data_eth.stanovnici[idxi]);
-                    }
-                    for (int idxi = 0; idxi < data_eth_zup.size; idxi++)
-                    {
-                        ime_holder_zup.GetChild(idxi).GetComponent<TextMeshProUGUI>().text = String.Format("{0}: {1}%, {2:N0}", data_eth_zup.ljudi[idxi], data_eth_zup.postotci[idxi] * 100f, data_eth_zup.stanovnici[idxi]);
-                    }
-
-                    update_rat_text();
-                    update_province_ui();
-                }
-                List<Vector2Int> pix_data = pixels_zup[target];
-
-                foreach (Vector2Int vector2Int in pix_data)
-                {
-                    fakeMap_dum.SetPixel(vector2Int.x, vector2Int.y, highlight);
-                }
-                fakeMap_dum.Apply();
-                map_mask.texture = fakeMap_dum;
-            }
-        }
-        foreach (ProvinceData data in selected_provinces.ToArray())
-        {
-            if (data.name != selected_province.name)
-            {
-                List<Vector2Int> pix_data_ = pixels_zup[data.color];
-                foreach (Vector2Int vector2Int in pix_data_)
-                {
-                    fakeMap_dum.SetPixel(vector2Int.x, vector2Int.y, Color.clear);
-                }
-                fakeMap_dum.Apply();
-                map_mask.texture = fakeMap_dum;
-                selected_provinces.Remove(data);
-                continue;
-            }
-            else
-            {
-                List<Vector2Int> pix_data_ = pixels_zup[data.color];
-                foreach (Vector2Int vector2Int in pix_data_)
-                {
-                    fakeMap_dum.SetPixel(vector2Int.x, vector2Int.y, sel_high);
-                }
-                fakeMap_dum.Apply();
-                map_mask.texture = fakeMap_dum;
-                continue;
-            }
-        }
+        slider.value = 0;
     }
+    #endregion
 }
